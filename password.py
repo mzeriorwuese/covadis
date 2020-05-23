8e0b7e7f6c0277bcd7da22f41af639e08ddc9d25
mzermichael <mzermichael4@gmail.com>
2020-05-23 11:03:40 +0100

    git rm password.py --cached

--- password.py
+++
@@ -1,4 +0,0 @@
-from passlib.hash import pbkdf2_sha256
-hash = pbkdf2_sha256.hash("BwzQp%_-ZS92$JLkDxLkVvhRxL%Hm%JaZE-c!S9-")
-#print(hash)
-print(pbkdf2_sha256.verify("BwzQp%_-ZS92$JLkDxLkVvhRxL%Hm%JaZE-c!S9-", hash))
